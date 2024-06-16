dfd735168cef9a5cbf6e2b6f90e1847420b8103e
Timur Gain <timur.gain@gmail.com>
2024-06-16 17:56:35 +0400

    fix: API_PATHS

--- infra/app.py
+++ infra/app.py
@@ -3,7 +3,11 @@ import os
 import aws_cdk as cdk
 from infra.static_site_stack import StaticSiteStack
 
+# 1. Init cdk app
 app = cdk.App()
+
+# 2. Create stack containing s3 bucket, cloudfront distribution, and bucket deployment
 StaticSiteStack(app, "WebTM-StaticWebsite")
 
+# 3. Generate AWS CloudFormation template
 app.synth()
